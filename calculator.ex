48b7cf7ae83a60e39db83d2cd9397b7afcbe99ff
root <root@DESKTOP-VCG2F7S.localdomain>
2021-06-03 17:00:09 +0900

    function add substract

---
+++ calculator.ex
@@ -0,0 +1,25 @@
+defmodule Calculator do
+  alias :timer, as: Timer
+
+  def squared(x) do
+    x * x
+  end
+
+  def product_of(x, y) do
+    x * y
+  end
+
+  def sum_of(x,y) do
+    x + y
+  end
+
+  def subtract(x,y)  when x >= y do
+    :timer.sleep(1_000)
+    x - y
+  end
+
+  def subtract(x,y) when x < y do
+    Timer.sleep(1_000)
+    "x>=yとなるように入力してください。"
+  end
+end
